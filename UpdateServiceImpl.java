31a931b142db921f2040d2fdfb51cf69ca8fccf8
LAPTOP-CDQ7LB9I\3170 <3172136447@qq.com>
2021-01-04 16:53:07 +0800

    no message

--- UpdateServiceImpl.java
+++ UpdateServiceImpl.java
@@ -1,5 +1,45 @@
+package com.omj.mall.service.Impl;
+
+import com.omj.mall.entity.Review;
+import com.omj.mall.mapper.ReviewMapper;
+import com.omj.mall.service.ReviewService;
+import com.omj.mall.util.PageUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service("reviewService")
+public class ReviewServiceImpl implements ReviewService {
+
+    @Resource
+    ReviewMapper reviewMapper;
+
+    /**
+     * 根据商品id查询当前商品的评价总数
+     * @param productId
+     * @return
+     */
+    @Override
+    public Integer getTotalByProductId(Integer productId) {
+        return reviewMapper.selectTotalByProductId(productId);
+    }
+
+    @Override
+    public List<Review> getListByProductId(Integer productId, PageUtil pageUtil) {
+        return reviewMapper.selectByProductId(productId,pageUtil);
+    }
+
+    @Override
+    public Integer getTotalByOrderItemId(Integer productOrderItemId) {
+        return reviewMapper.selectTotalByOrderItemId(productOrderItemId);
+    }
+
     @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
     @Override
     public boolean add(Review review) {
         return reviewMapper.insertOne(review)>0;
     }
+}
--- UserServiceImpl.java
+++ UserServiceImpl.java
@@ -1,5 +1,47 @@
+package com.omj.mall.service.Impl;
+
+import com.omj.mall.mapper.UserMapper;
+import com.omj.mall.service.UserService;
+import com.omj.mall.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Lenovo
+ */
+@Service("userService")
+public class UserServiceImpl  implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public User get(Integer userId) {
+        return userMapper.selectOne(userId);
+    }
+
+    @Override
+    public Integer getTotal(User user) {
+        return userMapper.selectTotal(user);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public boolean add(User user) {
+        return userMapper.insertOne(user)>0;
+    }
+
+
     //用户登陆
     @Override
     public User login(String userName, String userPassword) {
         return userMapper.selectByLogin(userName,userPassword);
+
+
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public boolean update(User user) {
+        return userMapper.updateOne(user)>0;
     }
+}
